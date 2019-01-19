ff8b06ba36855dd3f87aca1560c87508c80026c8
Guo shiwei <945270835@qq.com>
2019-01-19 09:39:14 +0000

    Updata action show in User modle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @micropost = @user.micropost
-
+    institute = @micropost.institute
+    case institute
+      when  "0"
+        @micropost.institute = "新疆理化所"
+      when  "1"
+        @micropost.institute = "计算技术所"
+      when  "2"
+        @micropost.institute = "软件所"
+      when  "3"
+        @micropost.institute = "网络中心"
+    end
   end
   
   def create
